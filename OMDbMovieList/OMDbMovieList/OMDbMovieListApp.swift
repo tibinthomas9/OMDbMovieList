19c2c4422ad4e79f5e9da12099c1199b507a5799
Tibin Thomas <tibinmutholy@gmail.com>
2024-05-08 13:36:27 -0400

    Project Init

---
+++ OMDbMovieList/OMDbMovieList/OMDbMovieListApp.swift
@@ -0,0 +1,17 @@
+//
+//  OMDbMovieListApp.swift
+//  OMDbMovieList
+//
+//  Created by Tibin Thomas on 2024-05-08.
+//
+
+import SwiftUI
+
+@main
+struct OMDbMovieListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
